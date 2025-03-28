900463ab8cff80516c9596c169c3155e86f7b9a1
Victor David Santos <victordavidsantos@gmail.com>
2025-03-28 15:31:53 -0300

    Implement Panel.

--- src/game.lua
+++ src/game.lua
@@ -3,18 +3,19 @@ require("lib.index")
 function love.load()
   -- debug.debug()
   love.graphics.setDefaultFilter("nearest", "nearest")
-  font = ImageFont.new("data/font/font.png", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÁÉÍÓÚÀÃÕÂÊÔÑÇáéíóúàãõâêôñç0123456789.,:;!?¡¿/\\()[]+-%'\"←→∞$ ", 1)
-  ddl = DropDownList.new(10, 10, {font = font, values = {"opt1", "opt2", "opt3"}, selected_index = 2, img_path = "data/img/button1.png", opt_img_path = "data/img/button1.png", scale = 2})
+  local font = ImageFont.new("data/font/font.png", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÁÉÍÓÚÀÃÕÂÊÔÑÇáéíóúàãõâêôñç0123456789.,:;!?¡¿/\\()[]+-%'\"←→∞$ ", 1)
+  local btn = Button.new(10, 0, {font = font, text = "Button", img_path = "data/img/button1.png", scale = 2, anchor = "right"})
+  panel = Panel.new(10, 10, 300, 300, {btn}, {img_path = "data/img/panel.png", tiled = true})
 end
 
 function love.update(dt)
   Mouse.update()
   KB.update()
-  ddl:update()
+  panel:update()
 
-  if KB.pressed("lctrl") then txt:set_enabled(not txt.enabled) end
+  if KB.pressed("lctrl") then panel:set_enabled(not panel.enabled) end
 end
 
 function love.draw()
-  ddl:draw()
+  panel:draw()
 end
