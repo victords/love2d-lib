e802782dd868ad3c61df7360730adb5322ed2d90
Victor David Santos <victordavidsantos@gmail.com>
2025-03-28 22:50:15 -0300

    Sprite and GameObject draw with color; fix GameObject scaling.

--- lib/graphics/sprite.lua
+++ lib/graphics/sprite.lua
@@ -38,7 +38,7 @@ function Sprite:animate(indices, interval)
   end
 end
 
-function Sprite:draw(scale_x, scale_y, angle, flip)
+function Sprite:draw(scale_x, scale_y, color, angle, flip)
   scale_x = scale_x or 1
   scale_y = scale_y or 1
   angle = angle and (angle * math.pi / 180)
@@ -46,5 +46,7 @@ function Sprite:draw(scale_x, scale_y, angle, flip)
   local origin_y = 0.5 * self.row_height
   local scale_x_factor = flip == "horiz" and -1 or 1
   local scale_y_factor = flip == "vert" and -1 or 1
+  if color then love.graphics.setColor(color) end
   love.graphics.draw(self.img, self.quads[self.img_index], self.x + scale_x * origin_x, self.y + scale_y * origin_y, angle, scale_x_factor * scale_x, scale_y_factor * scale_y, origin_x, origin_y)
+  if color then love.graphics.setColor(1, 1, 1) end
 end
--- src/game.lua
+++ src/game.lua
@@ -2,7 +2,8 @@ require("lib.index")
 
 function love.load()
   Image.setRetro()
-  sprite = GameObject.new(10, 10, 6, 10, "data/img/attack4.png", Vector.new(-6, -4), 4, 2)
+  sprite = Sprite.new(10, 10, "data/img/attack4.png", 4, 2)
+  go = GameObject.new(10, 50, 12, 20, "data/img/attack4.png", Vector.new(-6, -4), 4, 2)
 end
 
 function love.update(dt)
@@ -10,10 +11,7 @@ function love.update(dt)
 end
 
 function love.draw()
-  sprite:draw(2, 2, nil, "horiz")
-  love.graphics.rectangle("fill", sprite.x, sprite.y, sprite.w * 2, sprite.h * 2)
-  sprite.y = sprite.y + 28
-  sprite:draw(2, 2)
-  love.graphics.rectangle("fill", sprite.x, sprite.y, sprite.w * 2, sprite.h * 2)
-  sprite.y = sprite.y - 28
+  sprite:draw(2, 2, {0.3, 1, 0.3})
+  go:draw(2, 2, {1, 0.3, 0.3}, nil, "horiz")
+  love.graphics.rectangle("fill", go.x, go.y, go.w, go.h)
 end
